8a51a4ced690788f22780f4b2c5f1260bd6dc184
Mark Angelo Nool <markangelonool0@gmail.com>
2023-11-05 18:55:02 +0800

    update 4 on java ordering system

--- TakoyakiOrderingSystem.java
+++ TakoyakiOrderingSystem.java
@@ -1,209 +1,239 @@
 import java.util.Scanner;
-import java.text.DecimalFormat;
 
 public class TakoyakiOrderingSystem {
 
     private double totalCost = 0.0;
 
-    abstract static class MenuItem {
-        private String name;
-        private double price;
-
-        public MenuItem(String name, double price) {
-            this.name = name;
-            this.price = price;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public double getPrice() {
-            return price;
-        }
-
-        public abstract double calculateCost(int quantity);
-
-        public abstract String getCategory();
-    }
-
-    static class Takoyaki extends MenuItem {
-        public Takoyaki(String name, double price) {
-            super(name, price);
-        }
-
-        @Override
-        public double calculateCost(int quantity) {
-            return getPrice() * quantity;
-        }
-
-        @Override
-        public String getCategory() {
-            return "Flavors";
-        }
-    }
-
-    public void displayMenu(MenuItem[] items) {
+    public void displayMenu() {
         System.out.println("\t\t\t\t+=======================================+");
         System.out.println("\t\t\t\t            GELO'S TAKOYAKI MENU         ");
         System.out.println("\t\t\t\t Flavors:                                ");
-        for (int i = 0; i < items.length; i++) {
-            System.out.println("\t\t\t\t   " + (i + 1) + ". " + items[i].getName() + " Php. " + items[i].getPrice());
-        }
-        System.out.println("\t\t\t\t   " + (items.length + 1) + ". CANCEL");
+        System.out.println("\t\t\t\t   1. Classic 4pcs             Php. 55.00");
+        System.out.println("\t\t\t\t   2. Octopus 4pcs             Php. 65.00");
+        System.out.println("\t\t\t\t   3. Green Onion 4pcs         Php. 55.00");
+        System.out.println("\t\t\t\t   4. Cheese 4pcs              Php. 55.00");
+        System.out.println("\t\t\t\t   5. Bacon 4pcs               Php. 65.00");
+        System.out.println("\t\t\t\t   6. Crab 4pcs                Php. 65.00");
+        System.out.println("\t\t\t\t   7. CANCEL");
         System.out.println("\t\t\t\t+=======================================+");
-    }
-
-    public int getUserChoice(int min, int max) {
-        Scanner scan = new Scanner(System.in);
-        int choice = 0;
-        while (true) {
-            System.out.print("Your choice: ");
-            choice = scan.nextInt();
-            if (choice >= min && choice <= max) {
-                break;
-            } else {
-                System.out.println("Invalid choice. Please try again.");
-            }
-        }
-        return choice;
-    }
-
-    public int getQuantity() {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Order Quantity: ");
-        return scan.nextInt();
-    }
-
-    public void orderMenuItem(MenuItem item) {
-        displayMenu(new MenuItem[] { item });
-
-        int choice = getUserChoice(1, 7);
-
-        if (choice >= 1 && choice <= 6) {
-            System.out.println("You selected " + item.getName() + ".");
-            int orderQuantity = getQuantity();
-            double orderCost = item.calculateCost(orderQuantity);
-            totalCost += orderCost;
-            System.out.println("Total cost for " + item.getName() + ": Php. " + formatPrice(orderCost));
-
-            System.out.print("Would you like to add additional items? (Y/N): ");
-            Scanner scan = new Scanner(System.in);
-            String addItems = scan.next();
-
-            if (addItems.equalsIgnoreCase("Y")) {
-                addItems(item);
-            }
-        } else if (choice == 7) {
-            System.out.println("Order canceled.");
-        }
-    }
-
-    public void addItems(MenuItem selectedItem) {
-        while (true) {
-            System.out.println("What item would you like to add?");
-            System.out.println("1. Drink");
-            System.out.println("2. Add New " + selectedItem.getCategory());
-            System.out.println("3. Finish Order");
-
-            int choice = getUserChoice(1, 3);
-
-            switch (choice) {
-                case 1:
-                    displayDrinks();
-                    int drinkChoice = getUserChoice(1, 7);
-                    if (drinkChoice >= 1 && drinkChoice <= 6) {
-                        double[] drinkPrices = { 34.10, 34.50, 34.10, 31.10, 26.95, 26.95 };
-                        System.out.print("Drink Quantity: ");
-                        int drinkQuantity = getQuantity();
-                        double drinkCost = drinkPrices[drinkChoice - 1] * drinkQuantity;
-                        totalCost += drinkCost;
-                        System.out.println("You've added " + drinkQuantity + " " + formatDrink(drinkChoice) +
-                                " to your order. Total cost for drinks: Php. " + formatPrice(drinkCost));
-                    } else if (drinkChoice == 7) {
-                        System.out.println("Drink order canceled.");
-                    }
-                    break;
-
-                case 2:
-                    displayMenu(new MenuItem[] { selectedItem });
-                    int newItemChoice = getUserChoice(1, 7);
-                    if (newItemChoice >= 1 && newItemChoice <= 6) {
-                        orderMenuItem(selectedItem);
-                    } else if (newItemChoice == 7) {
-                        System.out.println("Add new " + selectedItem.getCategory() + " canceled.");
-                    }
-                    break;
-
-                case 3:
-                    return;
-
-                default:
-                    System.out.println("Invalid choice.");
-            }
-        }
-    }
-
-    public void displayTotalCost() {
-        System.out.println("Total cost for your order: Php. " + formatPrice(totalCost));
     }
 
     public void displayDrinks() {
         System.out.println("\t\t\t\t+=======================================+");
         System.out.println("\t\t\t\t                  Drinks                 ");
         System.out.println("\t\t\t\t Available canned:                       ");
-        String[] drinkMenu = {
-            "Coke Original 325ml",
-            "Coke Zero 325ml",
-            "Sprite 325ml",
-            "Royal 330ml",
-            "Pepsi 320ml",
-            "Mountain Dew 320ml"
-        };
-        double[] drinkPrices = { 34.10, 34.50, 34.10, 31.10, 26.95, 26.95 };
-        for (int i = 0; i < drinkMenu.length; i++) {
-            System.out.println("\t\t\t\t   " + (i + 1) + ". " + drinkMenu[i] + " Php. " + drinkPrices[i]);
-        }
-        System.out.println("\t\t\t\t   " + (drinkMenu.length + 1) + ". CANCEL");
+        System.out.println("\t\t\t\t   1. Coke Original 325ml      Php. 34.10");
+        System.out.println("\t\t\t\t   2. Coke Zero 325ml          Php. 34.50");
+        System.out.println("\t\t\t\t   3. Sprite 325ml             Php. 34.10");
+        System.out.println("\t\t\t\t   4. royal 330ml              Php. 31.10");
+        System.out.println("\t\t\t\t   5. Pepsi 320ml              Php. 26.95");
+        System.out.println("\t\t\t\t   6. Mountain Dew 320ml       Php. 26.95");
+        System.out.println("\t\t\t\t   7. CANCEL                             ");
         System.out.println("\t\t\t\t+=======================================+");
     }
 
-    public String formatPrice(double price) {
-        DecimalFormat df = new DecimalFormat("0.00");
-        return df.format(price);
+    public void orderTakoyaki(String flavor, double price) {
+        Scanner scanQuantity = new Scanner(System.in);
+        System.out.println("You selected " + flavor + " Takoyaki.");
+        System.out.print("Order Quantity: ");
+        int orderQuantity = scanQuantity.nextInt();
+
+        double orderCost = price * orderQuantity;
+
+        totalCost += orderCost;
+
+        System.out.println("Total cost for " + flavor + " Takoyaki: Php. " + orderCost);
+
+        System.out.print("Would you like to add additional items? (Y/N): ");
+        Scanner scanAddItems = new Scanner(System.in);
+        String addItems = scanAddItems.nextLine();
+
+        if (addItems.equalsIgnoreCase("Y")) {
+            do {
+                Scanner drinkOrAddTakoyaki = new Scanner(System.in);
+                System.out.println("What item would you like to add?");
+                System.out.println("1. Drink");
+                System.out.println("2. Add New Takoyaki Order");
+                int choice1or2 = drinkOrAddTakoyaki.nextInt();
+
+                switch (choice1or2) {
+                    case 1:
+                        displayDrinks();
+                        System.out.print("Select a drink (1-6) or 7 to cancel: ");
+                        int drinkChoice = drinkOrAddTakoyaki.nextInt();
+                        if (drinkChoice >= 1 && drinkChoice <= 6) {
+                            String[] drinkMenu = {
+                                    "Coke Original 325ml",
+                                    "Coke Zero 325ml",
+                                    "Sprite 325ml",
+                                    "Royal 330ml",
+                                    "Pepsi 320ml",
+                                    "Mountain Dew 320ml"
+                            };
+                            double[] drinkPrices = { 34.10, 34.50, 34.10, 31.10, 26.95, 26.95 };
+                            Scanner drinkQ = new Scanner(System.in);
+                            System.out.print("Drink Quantity: ");
+                            int drinkQuantity = drinkQ.nextInt();
+                            if (drinkQuantity == 0) {
+                                System.out.println("You've added a " + drinkMenu[drinkChoice - 1] + " to your order.");
+                                totalCost += drinkPrices[drinkChoice - 1];
+                            } else {
+                                System.out.println("You've added " + drinkQuantity + " " + drinkMenu[drinkChoice - 1]
+                                        + " to your order.");
+                                totalCost += drinkPrices[drinkChoice - 1] * drinkQuantity;
+                            }
+                        } else if (drinkChoice == 7) {
+                            System.out.println("Drink order canceled.");
+                        } else {
+                            System.out.println("Invalid choice.");
+                        }
+                        break;
+                    case 2:
+                        System.out.println("You've chosen to add a new takoyaki order.");
+                        displayMenu();
+                        Scanner newOrderScanner = new Scanner(System.in);
+                        System.out.print("Your choice for the new takoyaki order: ");
+                        int newOrderChoice = newOrderScanner.nextInt();
+
+                        switch (newOrderChoice) {
+                            case 1:
+                                orderTakoyaki("Classic", 55.00);
+                                break;
+                            case 2:
+                                orderTakoyaki("Octopus", 65.00);
+                                break;
+                            case 3:
+                                orderTakoyaki("Green Onion", 55.00);
+                                break;
+                            case 4:
+                                orderTakoyaki("Cheese", 55.00);
+                                break;
+                            case 5:
+                                orderTakoyaki("Bacon", 65.00);
+                                break;
+                            case 6:
+                                orderTakoyaki("Crab", 65.00);
+                                break;
+                            case 7:
+                                System.out.println("Order canceled.");
+                                break;
+                            default:
+                                System.out.println("Invalid choice.");
+                                break;
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid choice.");
+                        break;
+                }
+
+                // After adding items, ask if the user wants to add more
+                System.out.print("Would you like to add more items? (Y/N): ");
+                addItems = drinkOrAddTakoyaki.next();
+
+            } while (addItems.equalsIgnoreCase("Y"));
+
+            // Payment handling
+            System.out.println("Total cost for your order: Php. " + totalCost);
+            System.out.println("Select Payment method: ");
+            System.out.println("1. Cash");
+            System.out.println("2. Gcash or Maya");
+            Scanner paymentScanner = new Scanner(System.in);
+            int paymentChoice = paymentScanner.nextInt();
+
+            switch (paymentChoice) {
+                case 1:
+                    System.out.println("You've chosen to pay with Cash.");
+                    handleCashPayment();
+                    break;
+                case 2:
+                    System.out.println("You've chosen to pay with Gcash or Maya.");
+                    handleGcashOrMayaPayment();
+                    break;
+                default:
+                    System.out.println("Invalid payment method choice.");
+                    break;
+            }
+
+        }
+
+        if (addItems.equalsIgnoreCase("N")) {
+            // Payment handling
+            System.out.println("Total cost for your order: Php. " + totalCost);
+            System.out.println("Select Payment method: ");
+            System.out.println("1. Cash");
+            System.out.println("2. Gcash or Maya");
+            Scanner paymentScanner = new Scanner(System.in);
+            int paymentChoice = paymentScanner.nextInt();
+
+            switch (paymentChoice) {
+                case 1:
+                    System.out.println("You've chosen to pay with Cash.");
+                    handleCashPayment();
+                    break;
+                case 2:
+                    System.out.println("You've chosen to pay with Gcash or Maya.");
+                    handleGcashOrMayaPayment();
+                    break;
+                default:
+                    System.out.println("Invalid payment method choice.");
+                    break;
+            }
+        }
+
     }
 
-    public String formatDrink(int choice) {
-        String[] drinkMenu = {
-            "Coke Original 325ml",
-            "Coke Zero 325ml",
-            "Sprite 325ml",
-            "Royal 330ml",
-            "Pepsi 320ml",
-            "Mountain Dew 320ml"
-        };
-        return drinkMenu[choice - 1];
+    public void handleCashPayment() {
+        System.out.println("This is cash payment");
+        // Implement the logic for handling cash payment
+        // This can include taking the cash, calculating change, etc.
+        // You can add this logic here.
+    }
+
+    public void handleGcashOrMayaPayment() {
+        System.out.println("This is E=Wallet payment");
+        // Implement the logic for handling Gcash or Maya payment
+        // This can include scanning QR codes, verifying transactions, etc.
+        // You can add this logic here.
+    }
+
+    public double getTotalCost() {
+        return totalCost;
     }
 
     public static void main(String[] args) {
         TakoyakiOrderingSystem orderingSystem = new TakoyakiOrderingSystem();
-        MenuItem[] takoyakiFlavors = {
-            new Takoyaki("Classic 4pcs", 55.00),
-            new Takoyaki("Octopus 4pcs", 65.00),
-            new Takoyaki("Green Onion 4pcs", 55.00),
-            new Takoyaki("Cheese 4pcs", 55.00),
-            new Takoyaki("Bacon 4pcs", 65.00),
-            new Takoyaki("Crab 4pcs", 65.00)
-        };
-        orderingSystem.displayMenu(takoyakiFlavors);
-        int choice = orderingSystem.getUserChoice(1, takoyakiFlavors.length + 1);
+        orderingSystem.displayMenu();
+        Scanner scan = new Scanner(System.in);
 
-        if (choice >= 1 && choice <= takoyakiFlavors.length) {
-            MenuItem selectedTakoyaki = takoyakiFlavors[choice - 1];
-            orderingSystem.orderMenuItem(selectedTakoyaki);
-        } else if (choice == takoyakiFlavors.length + 1) {
-            System.out.println("Order canceled.");
+        System.out.print("Your choice: ");
+        int choice = scan.nextInt();
+
+        switch (choice) {
+            case 1:
+                orderingSystem.orderTakoyaki("Classic", 55.00);
+                break;
+            case 2:
+                orderingSystem.orderTakoyaki("Octopus", 65.00);
+                break;
+            case 3:
+                orderingSystem.orderTakoyaki("Green Onion", 55.00);
+                break;
+            case 4:
+                orderingSystem.orderTakoyaki("Cheese", 55.00);
+                break;
+            case 5:
+                orderingSystem.orderTakoyaki("Bacon", 65.00);
+                break;
+            case 6:
+                orderingSystem.orderTakoyaki("Crab", 65.00);
+                break;
+            case 7:
+                System.out.println("Order canceled.");
+                break;
+            default:
+                System.out.println("Invalid choice.");
+                break;
         }
-        orderingSystem.displayTotalCost();
     }
 }
