3d3866c9b7783b194a47d422b98b3a938cea5bb1
Mark Angelo Nool <markangelonool0@gmail.com>
2023-12-19 12:24:46 +0800

    new save on java

--- workingWithNumbers.java
+++ workingWithNumbers.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class workingWithNumbers {
     public static void main(String[] args) {
         int myInt = 5;
         double myDouble = 5.99;
         System.out.println(myInt);
         System.out.println(myDouble);
+
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        double num1 = scan.nextDouble();
+        System.out.println("Enter another number: ");
+        double num2 = scan.nextDouble();
+        System.out.println(num1 + num2);
+        scan.close();
+
     }
 }
