37b60242f6e6691c5923a8bbaf03119b74f05fd9
Mark Angelo Nool <markangelonool0@gmail.com>
2023-12-19 10:41:24 +0800

    added working with numbers.java

---
+++ workingWithNumbers.java
@@ -0,0 +1,8 @@
+public class workingWithNumbers {
+    public static void main(String[] args) {
+        int myInt = 5;
+        double myDouble = 5.99;
+        System.out.println(myInt);
+        System.out.println(myDouble);
+    }
+}
